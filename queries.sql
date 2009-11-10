f1a797363c967aa5037e06a0d100440d05f2874a
rjp <zimpenfish@gmail.com>
2009-11-10 09:07:26 +0000

    - add some EXIF queries

--- queries.sql
+++ queries.sql
@@ -3,12 +3,19 @@ interesting queries
 # media split
 select media, count(1) as count from photo group by media;
 
+## TAGS
 # makes/models by count
 select count(1) as count, tag.tag as make from photo, tag where photo.id=tag.id and tag.namespace='camera' and tag.predicate='make' group by tag.tag order by count desc;
 select count(1) as count, tag.tag as model from photo, tag where photo.id=tag.id and tag.namespace='camera' and tag.predicate='model' group by tag.tag order by count desc;
 
 # top tags (excluding machine tags)
 select count(1) as count, tag from tag where namespace is null group by tag order by count desc limit 25;
+
+## EXIF
+# makes/model by count
+select count(1) as count, a.raw as make, b.raw as model from exif a, exif b on a.id=b.id where a.tag='Make' and b.tag='Model' group by make, model order by count desc;
+select count(1) as count, a.raw as make from exif a where a.tag='Make' group by make order by count desc;
+select count(1) as count, a.raw as model from exif a where a.tag='Model' group by model order by count desc;
 
 # views
 select id, views, upload, taken from photo order by views desc limit 25; 
